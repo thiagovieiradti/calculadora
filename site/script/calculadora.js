753d11214d61ee62fb42c886588d13242a33cc87
[dti] Thiago Vieira Reginaldo <thiagovieiradti@gmail.com>
2019-06-01 03:19:33 -0300

    adicionada função de Subtração

--- site/script/calculadora.js
+++ site/script/calculadora.js
@@ -2,3 +2,8 @@ export function Somar(operadorEsquerda, operadorDireita){
     console.log(`Somando ${operadorEsquerda} + ${operadorDireita}: `); 
     return operadorEsquerda + operadorDireita;
 };
+
+export function Subtrair(operadorEsquerda, operadorDireita){
+    console.log(`Subtrair ${operadorEsquerda} - ${operadorDireita}: `); 
+    return operadorEsquerda - operadorDireita;
+};
--- site/script/scripts.js
+++ site/script/scripts.js
@@ -1,4 +1,5 @@
 import { helloWorld } from './hello-world.js';
-import { Somar } from './calculadora.js';
+import { Somar, Subtrair } from './calculadora.js';
 helloWorld();
 console.log(Somar(1,3));
+console.log(Subtrair(1,3));
