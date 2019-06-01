3fe30e0ef56a835a3f5acd58984912aa24229c66
[dti] Thiago Vieira Reginaldo <thiagovieiradti@gmail.com>
2019-06-01 04:05:49 -0300

    Adicionada função Dividir à calculadora

--- site/script/calculadora.js
+++ site/script/calculadora.js
@@ -2,3 +2,8 @@ export function Somar(operadorEsquerda, operadorDireita){
     console.log(`Somando ${operadorEsquerda} + ${operadorDireita}: `); 
     return operadorEsquerda + operadorDireita;
 };
+
+export function Dividir(operadorEsquerda, operadorDireita){
+    console.log(`Dividindo ${operadorEsquerda} / ${operadorDireita}: `); 
+    return operadorEsquerda / operadorDireita;
+};
--- site/script/scripts.js
+++ site/script/scripts.js
@@ -1,4 +1,5 @@
 import { helloWorld } from './hello-world.js';
-import { Somar } from './calculadora.js';
+import { Somar, Dividir } from './calculadora.js';
 helloWorld();
 console.log(Somar(1,3));
+console.log(Dividir(1,3));
