dc405b520c8c04d7d0cd3851b3503eb3adbfd595
[dti] Thiago Vieira Reginaldo <thiagovieiradti@gmail.com>
2019-06-01 03:27:02 -0300

    Adicionada função de multiplicar à calculadora

--- site/script/scripts.js
+++ site/script/scripts.js
@@ -1,4 +1,5 @@
 import { helloWorld } from './hello-world.js';
-import { Somar } from './calculadora.js';
+import { Somar, Multiplicar } from './calculadora.js';
 helloWorld();
 console.log(Somar(1,3));
+console.log(Multiplicar(4,3));
